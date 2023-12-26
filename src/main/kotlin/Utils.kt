5ba0d56a5c6142dd086fe58789c1b4f752052bb3
RAHIM <hrahim401@gamil.com>
2023-12-26 19:26:40 +0530

    After adding some more Functions to Utils.kt

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -49,7 +49,7 @@ fun InputStream.readDouble() = Double.fromBits(readLong())
 fun InputStream.readUTF() = readNBytes(readUShort()).decodeToString()
 
 fun Byte.toPInt() = toInt() and 0xFF
-fun ByteArray.putBytes(vararg vls:Byte, from:Int=0): ByteArray{
+fun ByteArray.putBytes(vararg vls:Byte, from:Int=0):ByteArray {
     val loopTill = size-from
     vls.forEachIndexed { index, byte ->
         if(index>=loopTill)
@@ -58,19 +58,20 @@ fun ByteArray.putBytes(vararg vls:Byte, from:Int=0): ByteArray{
     }
     return this
 }
-fun ByteArray.putShort(idx:Int=0, value: Short): ByteArray{
+fun ByteArray.putShort(idx:Int=0, value: Short): ByteArray {
     set(idx,(value.toInt() shr 8).toByte())
     set(idx+1,value.toByte())
     return this
 }
-fun ByteArray.putInt(idx:Int=0, value: Int): ByteArray{
+fun ByteArray.putInt(idx:Int=0, value: Int):ByteArray {
     set(idx,(value shr 24).toByte())
     set(idx+1,(value shr 16).toByte())
     set(idx+2,(value shr 8).toByte())
     set(idx+3,value.toByte())
     return this
 }
-fun ByteArray.putLong(idx:Int=0, value: Long): ByteArray{
+fun ByteArray.putFloat(idx:Int=0, value: Float) = putInt(idx, value.toBits())
+fun ByteArray.putLong(idx:Int=0, value: Long):ByteArray {
     set(idx,(value shr 56).toByte())
     set(idx+1,(value shr 48).toByte())
     set(idx+2,(value shr 40).toByte())
@@ -81,31 +82,32 @@ fun ByteArray.putLong(idx:Int=0, value: Long): ByteArray{
     set(idx+7,value.toByte())
     return this
 }
-fun ByteArray.putUTF(idx:Int=0, value: String): ByteArray{
+fun ByteArray.putDouble(idx:Int=0, value: Double) = putLong(idx, value.toBits())
+fun ByteArray.putUTF(idx:Int=0, value: String):ByteArray {
     val len = value.length.toShort()
     putShort(idx,len)
     value.encodeToByteArray()
         .copyInto(this,idx+2)
     return this
 }
-fun ByteArray.putString(idx:Int=0, value: String, len:Int=value.length): ByteArray{
+fun ByteArray.putString(idx:Int=0, value: String, len:Int=value.length):ByteArray {
     value.encodeToByteArray()
         .copyInto(this,idx,0,len)
     return this
 }
-fun ByteArray.putBString(idx:Int=0, value: String, maxLen:Int=value.length): Byte{
-    val len = min(maxLen,size-idx-1).toByte()
-    set(idx,len)
-    putString(idx+1,value,len.toInt())
-    return len
+fun ByteArray.putBString(idx:Int=0, value: String, maxLen:Int=value.length):Int {
+    val len = min(maxLen,size-idx-1)
+    set(idx,len.toByte())
+    putString(idx+1,value, len)
+    return len + 1
 }
-fun ByteArray.putSString(idx:Int=0, value: String, maxLen:Int=value.length): Short{
-    val len = min(maxLen,size-idx-2).toShort()
-    putShort(idx,len)
-    putString(idx+2,value,len.toInt())
-    return len
+fun ByteArray.putSString(idx:Int=0, value: String, maxLen:Int=value.length):Int {
+    val len = min(maxLen,size-idx-2)
+    putShort(idx,len.toShort())
+    putString(idx+2,value, len)
+    return len + 2
 }
-fun ByteArray.pad(idx: Int=0,length:Int): ByteArray{
+fun ByteArray.pad(idx: Int=0,length:Int):ByteArray {
     for(i in idx until (idx+length))
         set(i,0)
     return this
@@ -121,6 +123,7 @@ fun ByteArray.getInt(idx:Int=0):Int = getBInt(idx).shl(24)
         .or(getBInt(idx+1).shl(16))
         .or(getBInt(idx+2).shl(8))
         .or(getBInt(idx+3))
+fun ByteArray.getFloat(idx:Int=0):Float = Float.fromBits(getInt(idx))
 fun ByteArray.getLong(idx:Int=0):Long = getBLong(idx).shl(56)
         .or(getBLong(idx+1).shl(48))
         .or(getBLong(idx+2).shl(40))
@@ -129,14 +132,11 @@ fun ByteArray.getLong(idx:Int=0):Long = getBLong(idx).shl(56)
         .or(getBLong(idx+5).shl(16))
         .or(getBLong(idx+6).shl(8))
         .or(getBLong(idx+7))
-fun ByteArray.getString(idx:Int=0,length: Int=size)
-        = decodeToString(idx,idx+length)
-fun ByteArray.getBString(idx:Int=0)
-        = decodeToString(idx+1,idx+1+get(idx))
-fun ByteArray.getSString(idx:Int=0)
-        = decodeToString(idx+2,idx+2+getShort(idx))
-fun ByteBuffer.getUTF(pos:Int=position()) =
-    array().decodeToString(pos,pos + short)
+fun ByteArray.getDouble(idx:Int=0):Double = Double.fromBits(getLong(idx))
+fun ByteArray.getString(idx:Int=0,length: Int=size) = decodeToString(idx,idx+length)
+fun ByteArray.getBString(idx:Int=0) = decodeToString(idx+1,idx+1+get(idx))
+fun ByteArray.getSString(idx:Int=0) = decodeToString(idx+2,idx+2+getShort(idx))
+fun ByteBuffer.getUTF(pos:Int=position()) = array().decodeToString(pos,pos + short)
 fun String.toSArray() = ByteArray(length+2).apply {
     putSString(0,this@toSArray,length)
 }
