4311792260a6e621f76682c9f3b99b72948a1d59
RAHIM <hrahim401@gamil.com>
2024-01-07 23:10:50 +0530

    After including License

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023-2024 Rahim
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import java.io.*
 import java.nio.ByteBuffer
 import java.util.*
--- src/main/kotlin/bridgeCore/RequestBridge.kt
+++ src/main/kotlin/bridgeCore/RequestBridge.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023-2024 Rahim
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package bridgeCore
 
 import awaitTill
